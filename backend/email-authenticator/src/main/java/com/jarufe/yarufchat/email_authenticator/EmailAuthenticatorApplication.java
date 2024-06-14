52eed2ce8c22a506418551b1a3391fa80b789d27
Gabriel Alves Jarufe <gabrieljarufe1@gmail.com>
2024-06-14 13:57:17 -0300

    initial email authenticator commit

---
+++ backend/email-authenticator/src/main/java/com/jarufe/yarufchat/email_authenticator/EmailAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.jarufe.yarufchat.email_authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAuthenticatorApplication.class, args);
+	}
+
+}
---
+++ backend/email-authenticator/src/test/java/com/jarufe/yarufchat/email_authenticator/EmailAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarufe.yarufchat.email_authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
